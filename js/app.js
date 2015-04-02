6bffe7fa3d36affbb203342f8f4c899ac58a1eb7
Dale Greene <drgjr87@gmail.com>
2015-04-01 22:37:23 -0400

    Update to app.js file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+// Global variable to keep track of the amount of successful wins
+var level = 0;
+// Global variable to see whether or not the level value has suprassed the highest obtained
+var check = 0;
+// Variables to check to see if last reset was due to win or loss
+var stat, win = 0, loss = 1;
+
 // Enemies our player must avoid
 var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
@@ -9,7 +16,7 @@ var Enemy = function(x,y) {
     this.x = x;
     this.y = y;
     this.speed = Math.floor((Math.random() * 200) + 100);
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -22,12 +29,12 @@ Enemy.prototype.update = function(dt) {
         this.x = -100;
         this.speed = Math.floor((Math.random() * 200) + 100);
     }
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -37,7 +44,7 @@ var Player = function() {
     // Initial player location
     this.x = 200;
     this.y = 400;
-}
+};
 
 // Update player location according to button presses and/or game win/loss; add and look for rocks
 Player.prototype.update = function() {
@@ -45,7 +52,8 @@ Player.prototype.update = function() {
     var noleft = 0, noright = 0, noup = 0, nodown = 0;
 
     // Loop through rocks array to check for locations relative to the player
-    for (rock in rocks) {
+    for (var rock in rocks) {
+      if (rock !== null) {
         // Check if rock is to the left of player
         if (this.x - 25 >= rocks[rock].x - 50 && this.x - 25 <= rocks[rock].x + 50)
             if (this.y >= rocks[rock].y - 60 && this.y <= rocks[rock].y + 24)
@@ -62,16 +70,17 @@ Player.prototype.update = function() {
         if (this.y + 25 >= rocks[rock].y - 60 && this.y + 25 <= rocks[rock].y + 24)
             if (this.x >= rocks[rock].x - 50 && this.x <= rocks[rock].x + 50)
                 nodown++;
+      }
     }
 
     // Button presses (if there is no rock obstruction)
-    if(this.ctlKey === 'left' && this.x > 0 && noleft == 0)
+    if(this.ctlKey === 'left' && this.x > 0 && noleft === 0)
         this.x -= 25;
-    else if(this.ctlKey === 'right' && this.x != 400 && noright == 0)
+    else if(this.ctlKey === 'right' && this.x != 400 && noright === 0)
         this.x += 25;
-    else if(this.ctlKey === 'up' && noup == 0)
+    else if(this.ctlKey === 'up' && noup === 0)
         this.y -= 25;
-    else if (this.ctlKey === 'down' && this.y != 400 && nodown == 0)
+    else if (this.ctlKey === 'down' && this.y != 400 && nodown === 0)
         this.y += 25;
     
     // Limit button press
@@ -79,7 +88,7 @@ Player.prototype.update = function() {
 
     // Game reset when player reaches water
     if (this.y < 0) {
-        status = win;
+        stat = win;
         reset();
         // Stop at level 5
         if (level != 5){
@@ -95,17 +104,17 @@ Player.prototype.update = function() {
 
     // Set the check to be equal to the current level
     check = level;
-}
+};
 
 // Draw the player on the screen
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Read input key press
 Player.prototype.handleInput = function(key) {
     this.ctlKey = key;
-}
+};
 
 // Rocks on the ground
 var Rock = function() {
@@ -114,19 +123,21 @@ var Rock = function() {
     this.y = (Math.random() * 250);
 
     // Loop to make sure rocks don't show in the same relative location
-    for (rock in rocks) {
+    for (var rock in rocks) {
+      if (rock !== null) {
         while (this.x >= rocks[rock].x - 75 && this.x <= rocks[rock].x + 75 && this.y >= rocks[rock].y - 75 && this.y <= rocks[rock].y + 75) {
             this.x = (Math.random() * 400);
             this.y = (Math.random() * 250);
             rock = 0;
         }
+      }
     }
-}
+};
 
 // Place rock on the screen
 Rock.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Function to display rocks in proper order from front to back of screen
 var overlap = function(rocksArray) {
@@ -134,8 +145,9 @@ var overlap = function(rocksArray) {
     var tempx, tempy;
 
     // Loop to display rocks in proper order from front to back of screen
-    for (rock in rocksArray) {
-        for (i=parseInt(rock)+1; i<rocks.length; i++) {
+    for (var rock in rocksArray) {
+      if (rock !== null) {
+        for (var i=parseInt(rock)+1; i<rocks.length; i++) {
             if (rocksArray[rock].y > rocksArray[i].y) {
                 tempx = rocksArray[rock].x;
                 tempy = rocksArray[rock].y;
@@ -145,41 +157,35 @@ var overlap = function(rocksArray) {
                 rocksArray[i].y = tempy;
             }
         }
+      }
     }
-}
-
-// Global variable to keep track of the amount of successful wins
-var level = 0;
-// Global variable to see whether or not the level value has suprassed the highest obtained
-var check = 0;
-// Variables to check to see if last reset was due to win or loss
-var status, win = 0, loss = 1;
+};
 
 // Function to check player/enemy collisions
 var checkCollisions = function() {
-    for (enemy in allEnemies) {
+    for (var enemy in allEnemies) {
         if (player.x >= allEnemies[enemy].x - 30 && player.x <= allEnemies[enemy].x + 30) {
             if (player.y >= allEnemies[enemy].y - 30 && player.y <= allEnemies[enemy].y + 30) {
-                status = loss;
+                stat = loss;
                 reset();
             }
         }
     }
-}
+};
 
 // Reset function to reset player to original location
-reset = function() {
+var reset = function() {
     player.x = 200;
     player.y = 400;
-    if (level == 5 && status == win) {
+    if (level == 5 && stat == win) {
         // Pop out all existing rocks, push in new ones with new locations
-        for (i=0; i<level; i++)
+        for (var i=0; i<level; i++)
             rocks.pop();
         for (i=0; i<level; i++)
             rocks.push(new Rock());
     }
     overlap(rocks);
-}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
